f72af3ba351ad0ae42eb0bb3c5c014560c6a5419
rolmertelis <rolmertelis@gmail.com>
2024-08-16 21:38:33 -0300

    commit inicial

---
+++ src/main/java/com/ensino/fakedata/FakedataApplication.java
@@ -0,0 +1,13 @@
+package com.ensino.fakedata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakedataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakedataApplication.class, args);
+	}
+
+}
